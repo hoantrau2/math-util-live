366893718bef1f027d1baef1824e29b4ab390e21
hoantrau2 <hoan.duongmybk852002@hcmut.edu.vn>
2022-09-24 08:45:24 +0700

    first commit

---
+++ Source.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+#include<cstring>
+#include<array>
+#include<algorithm>
+#define MAX 10
+using namespace std;
+
+
+
+int  main()
+{ 
+	int value = 10;
+	void* ptr = &value;
+	//cout << *ptr << endl;
+	int* iptr = static_cast<int*>(ptr);
+	cout << *static_cast<int*>(ptr) << endl;
+	return 0;
+}
